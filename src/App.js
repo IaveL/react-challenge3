d74d89214f17d7371f601d166a210f2b284c3b57
Igor Avelino <igoravel21@gmail.com>
2022-02-11 16:59:33 -0300

    challenge completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css"
+import jaca from "./jaca.jpg"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    dados: [ 
+      {
+      $name: `Igor`,
+      age: 20,
+      favFood: `Macarrão a molho pesto (e coquinha gelada)`,
+      song: [`Everything You've Come To Expect - The Last Shadow Puppets`,`Batphone - Arctic Monkeys`,`Lemons - Brye`]
+      }
+    ]
+  }
+
+render(){
+  return( 
+  <div className="MainBox">
+    <h1>Meu nome é: {this.state.dados[0].$name}</h1>
+
+    <h2>Eu tenho: {this.state.dados[0].age} anos</h2>
+
+    <h3>E minha comida favorita é: {this.state.dados[0].favFood}</h3>
+    
+    <h2>E três músicas que gosto muito são:</h2>
+    
+    {this.state.dados.map( (item) => (
+              <ol>
+                  <li>{item.song[0]}</li>
+                  <li>{item.song[1]}</li>
+                  <li>{item.song[2]}</li>
+              </ol>
+          ) ) }
+
+      <h2>E minha fruta favorita:</h2>
+      <img className="jaca" src={jaca}/>
+      <h6>(Jaca)</h6>
+
+  </div>
+  )
+}
+
 }
 
 export default App;
